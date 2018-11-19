f704ddafde13bb99c4fe25e5add04a9d189aa4b2
Mario Salles <mariosalles@sowconnect.com>
2018-11-19 17:12:10 -0200

    Atualização do Projeto de Interface Tasy.

---
+++ SOW.Automation/SOW.Automation.Interface.Tasy/Windows/WindowBase.cs
@@ -0,0 +1,35 @@
+﻿using SOW.Automation.Common.Desktop;
+using SOW.Automation.Service.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOW.Automation.Interface.Tasy.Windows
+{
+    public class WindowBase
+    {
+        private DesktopService _automationService;
+        protected DesktopService AutomationService
+        {
+            get { return _automationService; }
+            set { _automationService = value; }
+        }
+
+        public WindowBase(DesktopDriverContextInfo driverContextInfo)
+        {
+            this.AutomationService = new DesktopService(driverContextInfo);
+        }
+
+        public void Open(string url, DesktopDriverContextInfo driverContextInfo)
+        {
+            this.AutomationService.BaseElement.OpenWindow("Identificação de Usuário", driverContextInfo.Timeout);
+        }
+
+        public void Close(DesktopDriverContextInfo driverContextInfo)
+        {
+            this.AutomationService.BaseElement.CloseProcess(driverContextInfo.Timeout);
+        }
+    }
+}
